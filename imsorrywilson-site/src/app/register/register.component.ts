897b12b573570386b6806fe5f53705b6d44bce5e
Anas Mohamad Daud <anasdaud1996@gmail.com>
2020-06-12 00:33:49 -0400

    edited nav bar on userpage & added upload img f(x)

--- imsorrywilson-site/src/app/register/register.component.ts
+++ imsorrywilson-site/src/app/register/register.component.ts
@@ -4,6 +4,7 @@ import { Users } from '../Users';
 import { Router } from '@angular/router';
 import { DomSanitizer } from '@angular/platform-browser';
 import { JsonPipe } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-register',
@@ -12,7 +13,7 @@ import { JsonPipe } from '@angular/common';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor(private router: Router, private register: RegisterService, private sanitizer: DomSanitizer) { }
+  constructor(private router: Router, private register: RegisterService, private httpClient: HttpClient, private sanitizer: DomSanitizer) { }
 
   user:Users = {
     id: 0,
@@ -29,6 +30,8 @@ export class RegisterComponent implements OnInit {
   fileChange(event){
     this.user.pic = event.target.files[0];
   }
+
+ 
   onSubmit(r){
     console.log(r.value);
     this.user.username = r.value.username;
@@ -37,14 +40,25 @@ export class RegisterComponent implements OnInit {
     this.user.lastName = r.value.lastName;
     this.user.email = r.value.email;
     //JSON.parse(this.user);
+    //JSON.stringify(this.user.pic)
+    this.register.registerUser(this.user).subscribe();
+    window.alert("Account Registered!");
+    this.router.navigate(['login']);
+  }
+
+  // Placeholder function for the image upload. Needs testing
+  onUpload(r){
+    console.log(r.value);
+    let username = r.value.username;
     let s = this.user.pic.name;
     let j = s.split(".");
     let fname = this.user.username + ".png";
     let imgData = new FormData();
     imgData.append('imagefile',this.user.pic,fname);
-    JSON.stringify(this.user.pic)
-    this.register.registerUser(this.user).subscribe();
-    window.alert("Account Registered!");
-    this.router.navigate(['login']);
+    this.httpClient.post("http://localhost:9000/user/updateprofilepic", imgData)
+    .subscribe(x =>{
+      console.log(x);
+    });
+    window.alert("Image Uploaded!");
   }
 }
---
+++ imsorrywilson-site/src/app/userpage/userpage.component.ts
@@ -0,0 +1,62 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UsersService } from '../users.service';
+import { Users } from '../Users';
+import { Posts } from '../Posts';
+
+
+@Component({
+  selector: 'app-userpage',
+  templateUrl: './userpage.component.html',
+  styleUrls: ['./userpage.component.css']
+})
+export class UserpageComponent implements OnInit {
+
+  constructor(private router: Router, private userservice: UsersService) { }
+
+  user:Users = {
+    id: 0,
+    username: "",
+    password: "",
+    firstName: "",
+    lastName: "",
+    email: "",
+    pic: null
+  }
+
+  home:number = 0; //0 for user page, 1 for settings page, 2 for search //3 for user profile page
+
+  post:Posts={
+    ID:0,
+    userID: this.user.id,
+    post:"",
+    pic: null,
+    postDate:""
+  }
+  
+  ngOnInit(): void {
+    this.user = this.userservice.getIndividualUser();
+  }
+  charactersRemaining = '190';
+  current = '';
+  updateCountdown(){
+    console.log(this.current)
+    this.charactersRemaining = (189 - this.current.length).toString();
+  }
+
+  homie(){
+    this.home = 0;
+  }
+  settings(){
+    this.home = 1;
+  }
+  search(){
+    this.home = 2;
+  }
+  profile(){
+    this.home = 3;
+  }
+  logout(){
+    this.router.navigate(['login']);
+  }
+}
